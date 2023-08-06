0365c3abe1b35eb84edf467baa0473454594f9ee
Remku <ngancloverr+remkugithub@gmail.com>
2023-08-06 19:05:24 +0530

    some comments added

--- routes/images.routes.js
+++ routes/images.routes.js
@@ -3,10 +3,13 @@ const router = require('express').Router()
 
 router.get('/:id', (req, res) => {
   const { id } = req.params
+
+  // TODO: This should not be there in production
   const ipAddr = os
     .networkInterfaces()
     .wlp13s0.find((item) => item.family === 'IPv4').address
 
+  // TODO: This line needs to be changed in production too
   const devImageUrl = `${req.protocol}://${ipAddr}:${req.socket.localPort}`
 
   console.log(`${devImageUrl}/images/${id}.jpg`)
